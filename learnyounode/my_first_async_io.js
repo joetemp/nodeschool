d8387a4e963e3006a0f5a9d19d1ba7ea2127f2e7
Joe Templeton <joe.k.templeton@gmail.com>
2015-10-16 22:26:26 -0600

    Added some notes to my_first_async_io.js

--- learnyounode/my_first_async_io.js
+++ learnyounode/my_first_async_io.js
@@ -1,10 +1,9 @@
-// What the hell is going on here? 'contents' is never defined and is that a fucntion inside a variable?
+var fs = require('fs')                                    			// This imports the fs lib.
+var file = process.argv[2]                                 			// This declares a variable called 'file' that holds the third arg given to this program.
 
-var fs = require('fs')
-var file = process.argv[2]
-
-fs.readFile(file, function (err, contents) {
-    var lines = contents.toString().split('\n').length - 1
-	console.log(lines)
+fs.readFile(file, function (err, contents) {						// This calls the readFile module within the fs lib. It give it a file for its first arg, and a newly created callback function as its second arg. The callback function takes two args, like all callback functions. Provided there is not an error, readFile will CALL BACK with the contents of the file and they will be routed into the var.
+	// fs.readFile(file, 'utf8', callback) can also be used
+    var lines = contents.toString().split('\n').length - 1			// This takes what readFile routed to 'contents' and turns it into a string. Then weeds out all the newlines, counts them and subtracts one.
+	console.log(lines)												// This prints it all to the console.
 })
 
