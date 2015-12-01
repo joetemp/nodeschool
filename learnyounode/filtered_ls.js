f7d99b5fbead06c77b2ab83080033606b6992338
Joe Templeton <joe.k.templeton@gmail.com>
2015-11-30 23:39:20 -0700

    Added notes for 'Filtered LS'

--- learnyounode/filtered_ls.js
+++ learnyounode/filtered_ls.js
@@ -1,9 +1,20 @@
-var fs = require('fs')
-var path = require('path')
+var fs = require('fs')                                      // 1
+var path = require('path')                                  // 2
 
-fs.readdir(process.argv[2], function (err, list) {
-    list.forEach(function (file) {
-        if (path.extname(file) === '.' + process.argv[3])
-	    console.log(file)
+fs.readdir(process.argv[2], function (err, list) {          // 3
+    list.forEach(function (file) {                          // 4
+        if (path.extname(file) === '.' + process.argv[3])   // 5
+	    console.log(file)                               // 6
     })
 })
+
+// Notes
+
+// 1. This delcares a variable that holes/imports the fs lib.
+// 2. This declares a variable that holds/imports the path lib. 
+/* 3. This calls the readdir module within the fs lib. It takes the third command line argument as its first parameter. It takes a newly  
+      created callback function as it's second param. As with all callback functions, this one has two parameters. The first one is there to 
+      catch errors. Provided there are no errors, readdir will CALL BACK with a list of files. That list will be routed into the var 'list'. */
+// 4. This runs a forEach loop over the list of files that readdir returned. The loop contains a function that takes each file as a param.
+// 5. If the path extensiton of the given as an arg is equal to the path extension given as the fourth command like arg...
+// 6. It's printed to the console.
--- learnyounode/my_first_async_io.js
+++ learnyounode/my_first_async_io.js
@@ -11,8 +11,8 @@ fs.readFile(file, function (err, contents) {                        // 3
 
 // 1. This imports the fs lib.
 // 2. This declares a variable called 'file' that holds the third arg given to this program.
-/* 3. This calls the readFile module within the fs lib. It give it a file for its first arg, and a newly created callback function as its 
-      second arg. The callback function takes two args, like all callback functions. Provided there is not an error, readFile will CALL BACK 
+/* 3. This calls the readFile module within the fs lib. It takes a file as it's first parameter, and a newly created callback function as its 
+      second param. The callback function takes two params, like all callback functions. Provided there is not an error, readFile will CALL BACK 
       with the contents of the file and they will be routed into the var. */
 // 4. This takes what readFile routed to 'contents' and turns it into a string. Then weeds out all the newlines, counts them and subtracts one.
 // 5. This prints it all to the console.
