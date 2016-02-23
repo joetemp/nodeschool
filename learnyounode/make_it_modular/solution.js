566134de4d8fd8bcb0914cd6bf0f2abf003b9766
Joe Templeton <joe.k.templeton@gmail.com>
2016-02-22 17:28:35 -0700

    make_it_modular is fully explained.

--- learnyounode/make_it_modular/solution.js
+++ learnyounode/make_it_modular/solution.js
@@ -13,11 +13,12 @@ filterFn(dir, filterStr, function (err, list) {             // 4
 
 // Notes
 
-// 1.
-// 2.
-// 3.
-// 4.
-// 5.
-// 6.
-// 7.
-// 8.
+// 1. This declares a variable that holds/imports the solution_filter.js file/program
+// 2. This declares a variable that holds the third command line argument, which is directory we'll be searching.
+// 3. This declares a variable that holds the fourth command line argument, the file extension we'll be listing.
+// 4. This is where we're actually calling/using the 'filterFn' program. It takes three params: the directory we'll be looking in, the file
+//    extensions we're searching for and a callback function that takes two params: an error and a list.
+// 5. If there is an error...
+// 6. Exit and log the error message to the console.
+// 7. This takes the list that solution_filter.js called back with and runs a function for each item. That function takes a file as a pram...
+// 8. And logs it to the console each time it's run.
--- learnyounode/make_it_modular/solution_filter.js
+++ learnyounode/make_it_modular/solution_filter.js
@@ -1,17 +1,17 @@
-var fs = require('fs')                                      // 1
-var path = require('path')                                  // 2
+var fs = require('fs')                                                // 1
+var path = require('path')                                            // 2
 
-module.exports = function (dir, filterStr, callback) {      // 3
+module.exports = function (dir, filterStr, callback) {                // 3
     
-    fs.readdir(dir, function (err, list) {                  // 4
-        if (err)                                            // 5
-            return callback(err)                            // 6
+    fs.readdir(dir, function (err, list) {                            // 4
+        if (err)                                                      // 5
+            return callback(err)                                      // 6
 
         list = list.filter(function (chickenNotFile) {                // 7
             return path.extname(chickenNotFile) === '.' + filterStr   // 8
         })
 
-        callback(null, list)                                // 9
+        callback(null, list)                                          // 9
     })
 }
 
@@ -21,10 +21,14 @@ module.exports = function (dir, filterStr, callback) {      // 3
 // 2. This declares a variable that holds/imports the path lib.
 /* 3. This uses the module.exports object, which is created by the built-in Module system to export a function that takes
       three parameters: the 'dir' var that is declared in 'solution.js', the 'filterStr' var that is declared in 'solution.js'
-      and the the callback function that is also declared in 'solution.js'. In this instance... 'callback' is just a place-holder. */
-// 4.
-// 5.
-// 6.
-// 7.
-// 8.
-// 9.
+      and the the callback function that is also declared in 'solution.js'. In this instance... 'callback' is just a place-holder. This
+      just makes it clear that I intend to pass a callback function as a param. Namely... the callback function at #4 in solution.js*/
+// 4. This uses the 'readdir' module in the 'fs' lib and takes two params, the 'dir' var we discussed earlier, as well as a callback function. The
+//    callback function itself takes two params as all do, an error and a list.
+// 5. If there's an error...
+// 6. Stop the program and call solution.js back with that result so it can log the error message.
+// 7. By default, 'list' or whatever you want to call it, is an array that  contains every file in the dir we told it to look in. This reassigns 
+//    the contents of the 'list' var by using the built-in filter() method. Now, list will only contain files that pass the test implemented by 
+//    the privided function. That function takes each file in the array as a param.
+// 8. This returns only files that match the 'filterStr' var/param. It also adds a '.' so your command line args can be pretty.
+// 9. This calls back solutions.js with a nice, pretty list for it to log to the console.
