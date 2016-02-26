e9891e3c59bf08db316d0f6acb9e56607574d4d6
Joe Templeton <joe.k.templeton@gmail.com>
2016-02-25 17:05:50 -0700

    Made some notes in the http client exc. Also changed some semantics in my_first_async_io.js

--- learnyounode/http_client.js
+++ learnyounode/http_client.js
@@ -7,8 +7,10 @@ http.get(process.argv[2], function (response) {     // 2
 })
 
 // Notes
-// 1.
-// 2.
-// 3.
-// 4.
-// 5.
+// 1. This imports the 'http' module.
+// 2. This calls the 'get' method from within the 'http' module and takes two parameters. The first parameter is the third command line argument.
+//    The second parameter is a callback function, though it doesn't look like a conventional Node callback. That's because the 'response' is a 
+//    'Node Stream object'. Node Streams are basically objects that emit events.
+// 3. This uses the 'setEncoding' method to encode the reponse to 'utf8'
+// 4. This is how you listen to events. This just means that as soon as data is emitted, it'll be logged to the console. 
+// 5. Likewise, as soon as an error is emitted, it'll be logged to the console.
