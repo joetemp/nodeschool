0ca95049c039e871eac420406c55422979d5d559
Joe Templeton <joe.k.templeton@gmail.com>
2016-02-26 16:30:24 -0700

    Got juggling_async all set up.

--- learnyounode/juggling_async.js
+++ learnyounode/juggling_async.js
@@ -1,28 +1,46 @@
+var http = require('http')                                      // 1
+var bl = require('bl')                                          // 2
+var results = []                                                // 3
+var count = 0                                                   // 4
 
-var http = require('http')
-var bl = require('bl')
-var results = []
-var count = 0
-
-function printResults () {
-      for (var i = 0; i < 3; i++)
-              console.log(results[i])
+function printResults () {                                      // 5
+      for (var i = 0; i < 3; i++)                               // 6
+              console.log(results[i])                           // 7
 }
 
-function httpGet (index) {
-    http.get(process.argv[2 + index], function (response) {
-        response.pipe(bl(function (err, data) {
-            if (err)
-                return console.error(err)
+function httpGet (index) {                                      // 8
+    http.get(process.argv[2 + index], function (response) {     // 9
+        response.pipe(bl(function (err, data) {                 // 10
+            if (err)                                            // 11
+                return console.error(err)                       // 12
 
-            results[index] = data.toString()
-            count++
+            results[index] = data.toString()                    // 13
+            count++                                             // 14
 
-            if (count == 3)
-                printResults()
-        }))
+            if (count == 3)                                     // 15
+                printResults()                                  // 16
+        }))                                                     
     })
 }
 
-for (var i = 0; i < 3; i++)
-  httpGet(i)
+for (var i = 0; i < 3; i++)                                     // 17
+  httpGet(i)                                                    // 18
+
+// 1.
+// 2.
+// 3.
+// 4.
+// 5.
+// 6.
+// 7.
+// 8.
+// 9.
+// 10.
+// 11.
+// 12.
+// 13.
+// 14.
+// 15.
+// 16.
+// 17.
+// 18.
